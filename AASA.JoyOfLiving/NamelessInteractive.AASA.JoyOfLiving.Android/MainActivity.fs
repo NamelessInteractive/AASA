dd6638cde527fa41fbfebccdedf6d2f7673baa1f
Kevin Ashton <zaowuya@gmail.com>
2014-08-03 12:41:33 +0200

    Initial working version.
    Does nothing yet, but at least compiles on all the platforms.

---
+++ AASA.JoyOfLiving/NamelessInteractive.AASA.JoyOfLiving.Android/MainActivity.fs
@@ -0,0 +1,20 @@
+﻿namespace NamelessInteractive.AASA.JoyOfLiving.Android
+
+open System
+
+open Android.App
+open Android.Content
+open Android.OS
+open Android.Runtime
+open Android.Views
+open Android.Widget
+
+[<Activity (Label = "Test", MainLauncher = true)>]
+type MainActivity () =
+    inherit Xamarin.Forms.Platform.Android.AndroidActivity ()
+    override this.OnCreate (bundle) =
+
+        base.OnCreate (bundle)
+
+        Xamarin.Forms.Forms.Init(this,bundle)
+        this.SetPage(NamelessInteractive.AASA.JoyOfLiving.Core.App.GetMainPage())
---
+++ AASA.JoyOfLiving/NamelessInteractive.AASA.JoyOfLiving.Core/Entry.fs
@@ -0,0 +1,14 @@
+﻿namespace NamelessInteractive.AASA.JoyOfLiving.Core
+
+open Xamarin.Forms
+
+type App() =
+    static member GetMainPage() =
+        ContentPage(
+            Content = 
+                Label(
+                    Text="Hello Xamarin.Forms!", 
+                    VerticalOptions = LayoutOptions.CenterAndExpand, 
+                    HorizontalOptions=LayoutOptions.CenterAndExpand
+                    )
+                )
